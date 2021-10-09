1ff58ea08325cef6b34468418e037616f81d19e8
Marina <lahniuk.m@gmail.com>
2021-10-09 21:04:59 +0300

    1 commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* const house={
+    rooms: ["kitchen", "bedroom", "hall"],
+    windows: 4,
+    square: 55,
+    balcon: true,
+    "main color": "white",
+};
+    
+
+
+console.log(house.rooms[0]);
+
+let x=2;
+console.log(4+ ++x);
+console.log(x); */
+
+const userName = prompt("Your name?", "");
+alert(`Your name is ${userName}`);
+
+
